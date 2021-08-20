46d3604520c92d95058162c1546bb59e36a40a7a
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2021-08-20 23:06:32 +0300

    test 100 brightness

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ LED_COUNT = 30        # Number of LED pixels.
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 100  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -22,31 +22,24 @@ def colorWipe(strip, color, wait_ms=50):
 
 if __name__ == '__main__':
     # Process arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    # args = parser.parse_args()
 
-    # Create NeoPixel object with appropriate configuration.
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
     print('strip created')
     strip.begin()
     print('strip initialized')
 
     print('Press Ctrl-C to quit.')
 
-    strip.setPixelColor(0, Color(0, 255, 0))
-    if not args.clear:
-        print('Use "-c" argument to clear LEDs on exit')
-
     try:
 
         while True:
             print('Set pixel')
-            strip.setPixelColor(0, Color(0, 255, 0))
+            strip.setPixelColor(0, Color(0, 0, 255))
             strip.show()
             time.sleep(1)
 
     except KeyboardInterrupt:
-        if args.clear:
-            colorWipe(strip, Color(0, 0, 0), 10)
+        colorWipe(strip, Color(0, 0, 0), 10)
