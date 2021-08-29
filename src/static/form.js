16fae07402e8e03735f38e3776f1d7af9c1a97e0
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2021-08-29 13:55:29 +0300

    fix url

---
+++ src/static/form.js
@@ -0,0 +1,43 @@
+const BASE_URL = 'http://192.168.100.43';
+const CHANGE_COLOR_URL = `${BASE_URL}/color`;
+
+const form = document.getElementById('colors');
+
+function addColorListener(inputId, outputId) {
+    const output = document.getElementById(outputId);
+    const input = document.getElementById(inputId);
+
+    output.value = parseInt(input.value);
+    form.addEventListener('input', () => {
+        output.value = parseInt(input.value);
+    });
+}
+
+function getColorValue(id) {
+    const output = document.getElementById(`${id}Output`);
+    return output.value;
+}
+
+addColorListener('red', 'redOutput');
+addColorListener('green', 'greenOutput');
+addColorListener('blue', 'blueOutput');
+
+const submit = document.getElementsByTagName('button')[0];
+submit.addEventListener('click', () => {
+    (async () => {
+        const rawResponse = await fetch(CHANGE_COLOR_URL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                r: getColorValue('red'),
+                g: getColorValue('green'),
+                b: getColorValue('blue'),
+            })
+        });
+        const content = await rawResponse.json();
+        console.log(content);
+    })();
+});
