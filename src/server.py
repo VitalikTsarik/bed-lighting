b19a62427629f3315140b0b5950c7855f2aaa9ca
VitalikTsarik <vitaliktsarik.forwork@gmail.com>
2021-09-03 21:23:03 +0300

    add test scheduled task

--- src/server.py
+++ src/server.py
@@ -1,8 +1,32 @@
 from flask import Flask, current_app, url_for, request, jsonify
+from flask_apscheduler import APScheduler
+from rpi_ws281x import Color
 
+from effects import colorWipe
 from led_controller import update_color_status, start_visualization
+from watcher import strip
+
+
+class Config:
+    SCHEDULER_API_ENABLED = True
+
 
 app = Flask(__name__)
+app.config.from_object(Config())
+
+scheduler = APScheduler()
+scheduler.init_app(app)
+
+
+@scheduler.task('cron', id='sunset', minute='*')
+def turn_on_lights():
+    if strip.getPixelColor(0) == Color(10, 0, 0):
+        colorWipe(strip, Color(0, 0, 10))
+    else:
+        colorWipe(strip, Color(10, 0, 0))
+
+
+scheduler.start()
 
 
 @app.before_first_request
